4b0f959017e8724d2b77caf589487fddc428043b
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2024-07-18 23:10:57 +0200

    working code reads from fake sensor and updates chart

--- src/components/SensorReadings.tsx
+++ src/components/SensorReadings.tsx
@@ -1,27 +1,57 @@
-import { useState } from "react";
-import LineChart from "./LineChart";
+import { useEffect, useRef, useState } from "react";
 import { Data } from "../utils/sample-data";
 import type { SensorClient } from "@viamrobotics/sdk";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Filler
+);
 
 export interface SensorReadingsProps {
   sensorClient?: SensorClient;
 }
 
+// Sensor reading structure
+interface SensorReading {
+  value?: number;
+  timestamp?: Date;
+}
+
 export const SensorReadings = (props: SensorReadingsProps): JSX.Element => {
+  const chartRef =
+    useRef<ChartJS<"line", (number | undefined)[], unknown>>(null);
   const { sensorClient } = props;
-  console.log("SensorClient");
-  if (sensorClient) {
-    sensorClient.getReadings().then((readings) => {
-      console.log(JSON.stringify(readings));
-    });
-  }
+  /*
+  const [sensorReadings, setSensorReadings] = useState<SensorReading[]>([
+    { value: 0, timestamp: new Date() },
+  ]);
+  
+*/
   // Charts Data
   const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.year),
+    labels: [], //sensorReadings.map((reading) => reading.timestamp),
     datasets: [
       {
         label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
+        data: [], //sensorReadings.map((reading) => reading.value),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#ecf0f1",
@@ -36,10 +66,27 @@ export const SensorReadings = (props: SensorReadingsProps): JSX.Element => {
     ],
   });
 
+  useEffect(() => {
+    //Implementing the setInterval method
+    const interval = setInterval(() => {
+      sensorClient?.getReadings().then((reading) => {
+        if (reading["a"] && typeof reading["a"] === "number") {
+          chartRef.current?.data.labels?.push(Date());
+          chartRef.current?.data.datasets?.[0]?.data?.push(
+            reading["a"] as number
+          );
+          chartRef.current?.update();
+        }
+      });
+    }, 1000);
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }); //[sensorReadings]);
+
   return (
     <div>
       <p>Sensor Readings</p>
-      <LineChart chartData={chartData} />
+      <Line ref={chartRef} data={chartData} />
     </div>
   );
 };
