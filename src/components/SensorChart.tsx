3d6721016e089df4526302a070809ff106c13a6a
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2024-07-22 17:55:57 +0200

    Update SensorChart.tsx

--- src/components/SensorChart.tsx
+++ src/components/SensorChart.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import type { SensorClient } from "@viamrobotics/sdk";
 import { LineChart } from "@mui/x-charts/LineChart";
 
+//Create constant of timestamps for initial chart data at one second interval for 10 data points
+const initTimestamps: SensorReading[] = Array.from({ length: 10 }, (_, i) => {
+  return { timestamp: new Date(Date.now() - 1000 * i) };
+});
+initTimestamps.reverse();
+
 export interface SensorReadingsMUIXProps {
   sensorClient?: SensorClient;
   seriesKeys: string[]; // Sensor reading keys to display
@@ -22,7 +28,7 @@ type SeriesData = {
 // Sensor readings component
 export const SensorChart = (props: SensorReadingsMUIXProps): JSX.Element => {
   const { sensorClient, seriesKeys } = props;
-  const [readings, setReadings] = useState<SensorReading[]>([]);
+  const [readings, setReadings] = useState<SensorReading[]>(initTimestamps);
 
   useEffect(() => {
     const intervall = setInterval(() => {
